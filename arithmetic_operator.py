58845fe2772e23b67ffc08da479a9d46eb81116e
godhaniya sumit <godhaniyasumit5@gmail.com>
2026-07-07 07:09:20 -0700

    Add files via upload

---
+++ arithmetic_operator.py
@@ -0,0 +1,9 @@
+a = 10
+b = 4
+print("Addition:", a + b) # addition 
+print("Subtraction:", a - b) #substraction 
+print("Multiplication:", a * b) # multiplication
+print("Division:", a / b) # division
+print("Modulus:", a % b) # modulus 
+print("Exponentiation:", a ** b) # exponentiation
+print("Integer Division:", a // b) # integer division
