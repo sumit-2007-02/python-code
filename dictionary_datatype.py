e3e7210b6a3ba625130769c77e01eaee8a29cef2
godhaniya sumit <godhaniyasumit5@gmail.com>
2026-07-07 05:55:15 -0700

    Add files via upload

---
+++ dictionary_datatype.py
@@ -0,0 +1,15 @@
+d = {1: "vipul", 2: "paras", 3: "nilesh", 4: "shailesh"}
+print(d)
+print(d[1])
+print(d[4])
+print(d.keys())
+print(d.values())
+
+thisdict = {"brand": "Ford", "model": "Mustang", "year": 1964, "year": 1964}
+print(thisdict)
+print(thisdict["model"])
+print(thisdict["year"])
+print(thisdict.keys())
+print(thisdict.values())
+print(len(thisdict))
+print(type(thisdict))
