417751e124bc4af7f2f5471ccfe178938b4049af
godhaniya sumit <godhaniyasumit5@gmail.com>
2026-07-07 07:32:38 -0700

    Add files via upload

---
+++ area_of_shape.py
@@ -0,0 +1,18 @@
+print("area of square:-")
+s = 5 
+print(s*s)
+
+print("area of rectangle:-")
+l = 10
+b = 5
+print(l*b)
+
+print("area of triangle:-")
+b = 10
+h = 5
+print(0.5*b*h)
+
+print("area of circle:-")
+r = 5
+pi = 3.14
+print(pi*r*r)
